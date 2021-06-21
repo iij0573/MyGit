be8f307966e086ce5fc14fc126e8581ba149e550
inji <iij0573@naver.com>
2021-06-21 22:22:01 +0900

    A,C change in devC branch

--- src/A.java
+++ src/A.java
@@ -3,6 +3,6 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("hello A");
 		System.out.println("A º¯°æ");
-		System.out.println("change in devB branch");
+		System.out.println("change in devC branch");
 	}
 }
